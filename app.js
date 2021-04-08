a729a49b937c38b4964185698159eaab093ab2e8
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-04-09 00:48:43 +0300

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+function trueAnswer(){
+let li = document.getElementById("t");
+   li.style.backgroundColor = "rgb(63, 192, 80)";
+}
+
+function wrongAnswer(){
+   let li = document.getElementById("f");
+   li.style.backgroundColor = "red";
+}
+
